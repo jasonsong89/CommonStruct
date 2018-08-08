b2d67a4e7a6e02597b4f1a1fea72586c08f79ef8
jasonsong89 <11605716+jasonsong89@users.noreply.github.com>
2018-08-08 14:54:45 +0800

    添加dal层

--- DAL/EmployeeDal.cs
+++ DAL/EmployeeDal.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommonStruct
+namespace DAL
 {
-    public class Class1
+    public class EmployeeDal
     {
     }
 }
